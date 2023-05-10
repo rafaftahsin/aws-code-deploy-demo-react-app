a682e6485aad6d9fde3ee5529ef0ff450ad05c70
Rafaf Tahsin <rafaftahsin@gmail.com>
2023-05-11 01:20:54 +0600

    that's all I can do now

---
+++ codedeploy-scripts/install_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo echo "Rafaf" > /root/txt
+#
+#sudo yum update
+#sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+#sudo . ~/.nvm/nvm.sh
+#sudo nvm install 16
+#sudo npm install pm2 -g
+#sudo npm install -g serve
+#sudo npm install
+#sudo npm run build
